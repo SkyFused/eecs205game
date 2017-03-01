1c149ced12367a7628cb52420455965eee4e56de
Mateusz Ryczek <skyfused@gmail.com>
2017-02-28 20:11:33 -0600

    Add ClearScreen PROC

--- game.asm
+++ game.asm
@@ -1,7 +1,7 @@
 ; #########################################################################
 ;
 ;   game.asm - Assembly file for EECS205 Assignment 4/5
-;
+;   Mateusz Ryczek - mrr958
 ;
 ; #########################################################################
 
@@ -19,26 +19,51 @@ include game.inc
 ;; Has keycodes
 include keys.inc
 
-	
 .DATA
 
 ;; If you need to, you can place global variables here
 
 
 .CODE
-	
 
 ;; Note: You will need to implement CheckIntersect!!!
 
 GameInit PROC
-	
-	ret         ;; Do not delete this line!!!
+
+	ret
 GameInit ENDP
 
 
 GamePlay PROC
-	
-	ret         ;; Do not delete this line!!!
+  ;; Clear the screen on each runthrough to prevent artifacts
+  INVOKE ClearScreen
+
+	ret
 GamePlay ENDP
+
+CheckIntersect PROTO STDCALL oneX:DWORD, oneY:DWORD, oneBitmap:PTR EECS205BITMAP, twoX:DWORD, twoY:DWORD, twoBitmap:PTR EECS205BITMAP
+
+  ret
+CheckIntersect ENDP
+
+;; Clear the screen
+ClearScreen PROC USES eax
+
+  ;; Set up loop for clearing
+  mov eax, ScreenBitsPtr
+  xor ebx, ebx
+
+clearLoop:
+  ;; Write the black color byte to the whole screen
+  mov (BYTE PTR [eax]), bl
+  inc eax
+
+  ;; 640 * 480 = 307,200 - 1 for zero index
+  ;; Loop through entire screen array
+  cmp eax, 307199
+  jle clearLoop
+
+  ret
+ClearScreen ENDP
 
 END
