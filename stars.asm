278f8a8f593095a2cdcc88161d3d38f1d20d6c60
Mateusz Ryczek <skyfused@gmail.com>
2017-03-07 20:17:18 -0600

    Updated pause screen star field

--- stars.asm
+++ stars.asm
@@ -42,6 +42,7 @@ DrawStarField PROC
 DrawStarField ENDP
 
 DrawPauseField PROC
+  ;; star header
   invoke DrawStar, 100, 10
   invoke DrawStar, 247, 15
   invoke DrawStar, 20,  20
@@ -60,23 +61,82 @@ DrawPauseField PROC
   invoke DrawStar, 350, 25
   invoke DrawStar, 390, 70
 
-  invoke DrawStar, 100, 410
-  invoke DrawStar, 247, 415
-  invoke DrawStar, 20,  420
-  invoke DrawStar, 600, 425
-  invoke DrawStar, 174, 430
-  invoke DrawStar, 420, 435
-  invoke DrawStar, 576, 440
-  invoke DrawStar, 99,  445
-  invoke DrawStar, 50,  450
-  invoke DrawStar, 512, 455
-  invoke DrawStar, 450, 460
-  invoke DrawStar, 30,  465
-  invoke DrawStar, 102, 470
-  invoke DrawStar, 247, 475
-  invoke DrawStar, 302, 455
-  invoke DrawStar, 350, 425
-  invoke DrawStar, 390, 470
+  ;; star footer
+  invoke DrawStar, 100, 360
+  invoke DrawStar, 247, 365
+  invoke DrawStar, 20,  370
+  invoke DrawStar, 600, 375
+  invoke DrawStar, 174, 380
+  invoke DrawStar, 420, 385
+  invoke DrawStar, 576, 390
+  invoke DrawStar, 99,  395
+  invoke DrawStar, 50,  400
+  invoke DrawStar, 512, 405
+  invoke DrawStar, 450, 410
+  invoke DrawStar, 30,  415
+  invoke DrawStar, 102, 420
+  invoke DrawStar, 247, 425
+  invoke DrawStar, 302, 405
+  invoke DrawStar, 350, 375
+  invoke DrawStar, 390, 420
+
+  ;; star left side
+  invoke DrawStar, 10,  120
+  invoke DrawStar, 15,  420
+  invoke DrawStar, 21,  367
+  invoke DrawStar, 28,  334
+  invoke DrawStar, 30,  213
+  invoke DrawStar, 15,  405
+  invoke DrawStar, 59,  403
+  invoke DrawStar, 65,  100
+  invoke DrawStar, 70,  125
+  invoke DrawStar, 77,  344
+  invoke DrawStar, 83,  169
+  invoke DrawStar, 87,  225
+  invoke DrawStar, 95,  315
+  invoke DrawStar, 100, 375
+  invoke DrawStar, 115, 402
+  invoke DrawStar, 120, 275
+  invoke DrawStar, 135, 250
+  invoke DrawStar, 140, 219
+  invoke DrawStar, 146, 284
+  invoke DrawStar, 153, 117
+  invoke DrawStar, 161, 400
+  invoke DrawStar, 167, 225
+  invoke DrawStar, 170, 114
+  invoke DrawStar, 177, 198
+  invoke DrawStar, 183, 174
+  invoke DrawStar, 187, 313
+  invoke DrawStar, 195, 305
+
+  ;; star right side
+  invoke DrawStar, 630,  120
+  invoke DrawStar, 625,  420
+  invoke DrawStar, 619,  367
+  invoke DrawStar, 612,  334
+  invoke DrawStar, 610,  213
+  invoke DrawStar, 625,  405
+  invoke DrawStar, 589,  403
+  invoke DrawStar, 575,  100
+  invoke DrawStar, 570,  125
+  invoke DrawStar, 563,  344
+  invoke DrawStar, 557,  169
+  invoke DrawStar, 553,  225
+  invoke DrawStar, 545,  315
+  invoke DrawStar, 540, 375
+  invoke DrawStar, 525, 402
+  invoke DrawStar, 520, 275
+  invoke DrawStar, 505, 250
+  invoke DrawStar, 500, 219
+  invoke DrawStar, 494, 284
+  invoke DrawStar, 487, 117
+  invoke DrawStar, 479, 400
+  invoke DrawStar, 473, 225
+  invoke DrawStar, 470, 114
+  invoke DrawStar, 463, 198
+  invoke DrawStar, 457, 174
+  invoke DrawStar, 453, 313
+  invoke DrawStar, 445, 305
 
 	ret
 DrawPauseField ENDP
